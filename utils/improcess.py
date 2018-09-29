17a5e450c9eae63d954a7db57f7e036bb79b65c6
Will <William@William-2.local>
2018-09-29 14:40:39 -0400

    making sure it's all there

--- utils/improcess.py
+++ utils/improcess.py
@@ -7,3 +7,11 @@ def dicom_to_array(file_path,file_name):
     image = pydicom.dcmread(full_file)
 
     return image.pixel_array
+
+def dicom_to_normalized_array(file_path,file_name):
+
+    full_file = file_path+file_name
+
+    image = pydicom.dcmread(full_file)
+
+    return image.pixel_array/255
